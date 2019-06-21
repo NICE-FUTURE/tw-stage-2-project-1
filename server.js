4cc9a1ad67852e445bb817219a3792517cda3b0e
NICE-FUTURE <2375558167@qq.com>
2019-06-21 22:31:44 +0800

    compelete task

--- server.js
+++ server.js
@@ -8,9 +8,66 @@ let dbClient;
 
 MONGO_CLIENT.connect(DB_URL, {useNewUrlParser: true}, function (err, db) {
     if (err) throw err;
-    console.log("数据库已创建!");
+    console.log("数据库已连接!");
     dbClient = db.db("todolist");
 });
+
+function pushTodolist(response, data) {
+    if (data.opt === "todo") {
+        dbClient.collection("todo").insertOne({'data': data.data}, (err, result) => {
+            if (err) throw err;
+            console.log("insert one todo successfully.");
+            response.end();
+        });
+    }
+    else if (data.opt === "done") {
+        dbClient.collection("done").insertOne({'data': data.data}, (err, result) => {
+            if (err) throw err;
+            dbClient.collection("todo").deleteOne({'data': data.data}, (err, result)  => {
+                if (err) throw err;
+                console.log("done one from todo succesfully.");
+                response.end();
+            });
+        });
+    }
+    else if (data.opt === "delete") {
+        dbClient.collection("todo").deleteOne({'data': data.data}, (err, result)  => {
+            if (err) throw err;
+            console.log("delete one from todo succesfully.");
+            response.end();
+        });
+    }
+    else if (data.opt === "clear") {
+        dbClient.collection("done").deleteMany({}, (err, result)  => {
+            if (err) throw err;
+            console.log("delete all from done succesfully.");
+            response.end();
+        });
+    }
+    else if (data.opt === "reset") {
+        dbClient.collection("done").deleteOne({'data': data.data}, (err, result)  => {
+            if (err) throw err;
+            dbClient.collection("todo").insertOne({'data': data.data}, (err, result)  => {
+                if (err) throw err;
+                console.log("reset one from done succesfully.");
+                response.end();
+            });
+        });
+    }
+}
+
+function pullTodolist(response) {
+    dbClient.collection("todo").find({}, {'projection': {'_id': 0}}).toArray((todoErr, todos) => {
+        if (todoErr) throw err;
+        dbClient.collection("done").find({}, {'projection': {'_id': 0}}).toArray((doneErr, dones) => {
+            if (doneErr) throw err;
+            response.end(JSON.stringify(
+                {'todo': todos.map((ele)=>{return Object.values(ele)[0]}),
+                 'done': dones.map((ele)=>{return Object.values(ele)[0]})}
+            ));
+        });
+    });
+}
 
 let server = HTTP.createServer(function (request, response) {
     console.log(request.method + ': ' + request.url);
@@ -25,18 +82,36 @@ let server = HTTP.createServer(function (request, response) {
             response.end();
         });
     } else if (request.url === '/todo') {
+         // Set CORS headers
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+        response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+        let data = "";
         switch (request.method) {
             case 'POST':
-
+                console.log("post!");
                 break;
             case 'GET':
-
+                console.log("get!");
+                pullTodolist(response);
                 break;
             case 'PUT':
-
+                data = "";
+                console.log("put!");
+                request.on("data", (chunk)=>{
+                    data += chunk;
+                });
+                request.on("end", () => {
+                    data = JSON.parse(data);
+                    pushTodolist(response, data);
+                });
                 break;
             case 'DELETE':
-
+                console.log("delete!");
+                break;
+            case 'OPTIONS':
+                response.writeHead(200);
+                response.end();
                 break;
         }
     }
--- statics/index.js
+++ statics/index.js
@@ -5,7 +5,8 @@ window.onload = function () {
 
     let msgDiv = $('#msg_div');
     let todoInput = $('#todo_input');
-    let todoTable = $('#todo_table');
+    let todoTable = $('#todo_table tbody');
+    let doneTable = $('#done_table tbody');
 
     let allBtn = $('#all_btn');
     let activeBtn = $('#active_btn');
@@ -13,27 +14,93 @@ window.onload = function () {
     let clearCompleteBtn = $('#clear_complete_btn');
 
     let leftItemsSpan = $('#left_items_span');
+    let todoList = [];
 
-    let leftItemsCount = 0;
-    let todoListLocal = [];
-
-    todoInput.keydown(function () {
-
+    todoInput.keydown(function (e) {
+        if (e.which === 13) {
+            let todo = todoInput.val();
+            if (todo !== "") {
+                todoInput.val("");  // clear input field
+                pushData("todo", todo);
+            }
+        }
     });
 
     allBtn.click(function () {
-
+        todoTable.css("display", "table-row-group");
+        doneTable.css("display", "table-row-group");
     });
 
     activeBtn.click(function () {
-
+        todoTable.css("display", "table-row-group");
+        doneTable.css("display", "none");
     });
 
     completeBtn.click(function () {
-
+        todoTable.css("display", "none");
+        doneTable.css("display", "table-row-group");
     });
 
     clearCompleteBtn.click(function () {
-
+        pushData("clear", "");
     });
+
+    function pushData(opt, data) {
+        $.ajax({
+            url: HOST+":"+PORT+'/todo',
+            type: 'PUT',
+            contentType: "text/plain",
+            data: JSON.stringify({'opt': opt, 'data':data}),
+            success: ()=>{updateInfo();}
+        });
+    }
+
+    function updateInfo() {
+        $.ajax({
+            url: HOST+":"+PORT+"/todo",
+            type: "GET",
+            success: (res) => {
+                todoList = JSON.parse(res);
+                addTodo();
+                addDone();
+                leftItemsSpan.html("Left items: "+todoList.todo.length);
+                if (todoList.done.length > 0) {
+                    $("#clear_complete_btn").css("display", "inline-block");
+                }
+            }
+        });
+    }
+
+    function addTodo() {
+        todoTable.html("");
+        for (let item of todoList.todo) {
+            todoTable.append(`
+            <tr>
+                <td class='item'><li>${item}</li></td>
+                <td class='close_icon'>X</td>
+            </tr>`)
+        }
+        $(".close_icon").on("click", (e)=>{
+            pushData("delete", e.target.parentElement.firstElementChild.textContent);
+        });
+        $("#todo_table tbody tr .item").on("click", (e)=>{
+            pushData("done", e.target.textContent);
+        });
+    }
+
+    function addDone() {
+        doneTable.html("");
+        for (let item of todoList.done) {
+            doneTable.append(`
+            <tr>
+                <td><li class='item'>${item}</li></td>
+                <td class='close_icon'></td>
+                </tr>`)
+        }
+        $("#done_table tbody tr").on("click", (e)=>{
+            pushData("reset", e.target.textContent);
+        });
+    }
+
+    updateInfo();
 };
